9a83c4bd48ae9ecd259ce2e1dc65877ed9cce5b1
Jalen-art-war <jalencharles.26@gmail.com>
2021-06-15 11:54:49 +0000

    add platforms and collectables to game

--- projects/platformer/js/init/collectable.js
+++ projects/platformer/js/init/collectable.js
@@ -37,7 +37,7 @@
         // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
         
         // example: 
-        createCollectable(type.steve, 200, 170, 6, 0.7);
+        createCollectable(type.steve, 200, 180, 7, 0.7);
         
         
         
